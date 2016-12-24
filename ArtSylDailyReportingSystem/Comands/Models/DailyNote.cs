ca31c13f34cc13e2626d76d70f8080d1e800cd65
sator <sator@SATOR-PC>
2016-12-24 15:23:34 +0200

    Add constructor to dailyNote

--- ArtSylDailyReportingSystem/Comands/Models/DailyNote.cs
+++ ArtSylDailyReportingSystem/Comands/Models/DailyNote.cs
@@ -11,5 +11,12 @@ namespace ArtSylDailyReportingSystem.Comands.Models
         public DateTime CameInTime { get; set; }
         public DateTime CameOutTime { get; set; }
         public List<Task> Tasks { get; set; }
+
+        public DailyNote(DateTime CameInTime, DateTime CameOutTime, List<Task> Tasks )
+        {
+            this.CameInTime = CameInTime;
+            this.CameOutTime = CameOutTime;
+            this.Tasks = Tasks;
+        }
     }
 }
